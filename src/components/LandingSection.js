937e90ff548c580fe398b781988833c9e2366dc5
Robert Horosewski <horoserp@yahoo.com>
2023-10-12 21:11:00 -0400

    LandingSectoin Update: Add bg image. Add shadow and blur text effects. Increase greeting size.

--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { Avatar, Heading, VStack } from "@chakra-ui/react";
 import FullScreenSection from "./FullScreenSection";
-import headshot from '../images/Headshot.png'
+import headshot from '../images/Headshot.png';
+import backGround from '../images/Home-BG-Photo.jpg'
 
 const greeting = "Hello, I am Robert!";
 const bio1 = "A frontend developer";
@@ -12,7 +13,11 @@ const LandingSection = () => (
     justifyContent="center"
     alignItems="center"
     isDarkBackground
-    backgroundColor="#2A4365"
+    // backgroundColor="#2A4365"
+    backgroundImage={backGround}
+    backgroundSize="cover"
+    width="100vw"
+    maxWidth="100%"
     >
     {/* <Avatar name="Robert Horosewski" minH={150} maxW={150} size='2xl' src={headshot} />{' '} */}
     <VStack spacing={16}> 
@@ -24,7 +29,7 @@ const LandingSection = () => (
          minH={150}
          maxW={150}
        /> 
-       <Heading as="h4" size="md" noOfLines={1}> 
+       <Heading as="h4" size="xl" noOfLines={1} textShadow={"2px 2px 5px purple"}> 
          {greeting} 
        </Heading> 
      </VStack> 
