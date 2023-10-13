a72ca44309387e584a29931d5695f60435877e93
Robert Horosewski <horoserp@yahoo.com>
2023-10-12 21:37:22 -0400

    Update: Add links to my social media sites. Reorder social media sites.

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,23 +12,23 @@ import { Box, HStack } from "@chakra-ui/react";
 const socials = [
   {
     icon: faEnvelope,
-    url: "mailto: hello@example.com",
+    url: "mailto: robert.horosewski@gmail.com",
   },
   {
     icon: faGithub,
-    url: "https://github.com",
+    url: "https://github.com/horoserp/",
   },
   {
     icon: faLinkedin,
-    url: "https://www.linkedin.com",
-  },
-  {
-    icon: faMedium,
-    url: "https://medium.com",
+    url: "https://www.linkedin.com/in/robert-horosewski-8a0608196/",
   },
   {
     icon: faStackOverflow,
-    url: "https://stackoverflow.com",
+    url: "https://stackoverflow.com/users/12617284/horoserp",
+  },
+  {
+    icon: faMedium,
+    url: "https://medium.com/@horoserp",
   },
 ];
 
