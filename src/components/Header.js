5cbd4a2d0e0dfb9ae95973a43b1752e2e4c1f4aa
Robert Horosewski <horoserp@yahoo.com>
2024-02-21 16:42:58 -0500

    Update: Added hamburger menu for additional mobile responsiveness.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import {
@@ -7,7 +7,8 @@ import {
   faMedium,
   faStackOverflow,
 } from "@fortawesome/free-brands-svg-icons";
-import { Box, HStack, Stack } from "@chakra-ui/react";
+import { Box, HStack, Stack, Flex, IconButton, Spacer } from "@chakra-ui/react";
+import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
 
 const socials = [
   {
@@ -76,6 +77,8 @@ const Header = () => {
     }
   };
 
+  const [display, changeDisplay] = useState('none');
+
   return (
     <Box
       position="fixed"
@@ -89,42 +92,106 @@ const Header = () => {
       backgroundColor="#18181b"
       ref={headerRef}
     >
-      <Box color="white" maxWidth="1280px" margin="0 auto">
-        <Stack
-          direction={["column", "row", "row", "row"]}
-          justify={["center", "space-between", "flex-end", "flex-end"]}
-          px={16}
-          py={4}
-          justifyContent="space-between"
-          alignItems="flex-start"
+      <Flex>
+        <Flex
+          position="fixed"
+          display={[ 'none', 'none', 'flex', 'flex' ]}
+          align="center"
+          justifyContent="center"
+          direction={[ 'column', 'row', 'row', 'row']}
+          color="white"
+          backgroundColor="#18181b"
+          width="100%"
+          p={5}
         >
-          <nav>
-            <HStack
-              spacing={8}
-            >
-              {socials.map(({ icon, url }) => (
-               <a
-                 key={url}
-                 href={url}
-                 target="_blank"
-                 rel="noopener noreferrer"
-               >
-                 <FontAwesomeIcon icon={icon} size="2x" key={url} />
-               </a>
-              ))}
-            </HStack>
-          </nav>
-          <nav>
-            <HStack
-              spacing={8}
-            >
-              <a href="#top" onClick={handleClick('top')}>Home</a>
-              <a href="#projects" onClick={handleClick('projects')}>Projects</a>"
-              <a href="#contactme" onClick={handleClick('contactme')}>Contact Me</a>
-            </HStack>
-          </nav>
-        </Stack>
-      </Box>
+          <Flex
+            ml={"10%"}
+            display="flex"
+            direction={[ 'column', 'row', 'row', 'row' ]}
+            alignItems="center"
+            gap={16}
+          >
+            {socials.map(({ icon, url }) => (
+              <a
+                key={url}
+                href={url}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FontAwesomeIcon icon={icon} size="2x" key={url} />
+              </a>
+            ))}
+          </Flex>
+          <Spacer/>
+          <Flex
+            mr={"10%"}
+            display="flex"
+            gap={16}
+            direction={[ 'column', 'row', 'row', 'row' ]}
+            alignItems="center"
+          >
+            <a href="#top" onClick={handleClick('top')}>Home</a>
+            <a href="#projects" onClick={handleClick('projects')}>Projects</a>
+            <a href="#contactme" onClick={handleClick('contactme')}>Contact Me</a>
+          </Flex>
+        </Flex>
+        {/* Moblie */}
+        <IconButton
+          size="lg"
+          // ml={100}
+          icon={<HamburgerIcon/>}
+          onClick={() => changeDisplay("flex")}
+          display={[ 'flex', 'flex', 'none', 'none' ]}
+          colorScheme="black"
+        />
+        {/* Moblie Content */}
+        <Flex
+          display={display}
+        >
+          <Flex>
+            <IconButton
+              size="lg"
+              icon={<CloseIcon/>}
+              onClick={() => changeDisplay('none')}
+              colorScheme="black"
+            />
+          </Flex>
+          <Flex
+            display="flex"
+            gap={4}
+            pl={6}
+            pt={5}
+            direction="column"
+            alignItems="center"
+            color="white"
+          >
+            <a href="#top" onClick={handleClick('top')}>Home</a>
+            <a href="#projects" onClick={handleClick('projects')}>Projects</a>
+            <a href="#contactme" onClick={handleClick('contactme')}>Contact Me</a>
+          </Flex>
+          <Flex
+            display="flex"
+            direction="column"
+            alignItems="center"
+            gap={4}
+            pl={10}
+            pt={5}
+            pb={5}
+            color="white"
+          >
+            {socials.map(({ icon, url }) => (
+              <a
+                key={url}
+                href={url}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FontAwesomeIcon icon={icon} size="2x" key={url} />
+              </a>
+            ))}
+          </Flex>
+        </Flex>
+      </Flex>
     </Box>
   );
 };
