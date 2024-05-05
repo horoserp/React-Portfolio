e4911874aa41f1f21d90bcefb0f8a74a45d54ecb
Robert Horosewski <horoserp@yahoo.com>
2024-05-04 23:26:45 -0400

    Fix: Spelling error.

--- src/components/ProjectsSection.js
+++ src/components/ProjectsSection.js
@@ -21,7 +21,7 @@ const projects = [
  {
    title: "Bootstrap 5 Tutorial",
    description:
-     "This page is build using Bootstrap 5, and demonstrates the information I acquired while becoming a Certified Bootstrap 5 Developer. It illustrates multiple components and how to code them, as well as listing definitions and other key information.",
+     "This page is built using Bootstrap 5, and demonstrates the information I acquired while becoming a Certified Bootstrap 5 Developer. It illustrates multiple components and how to code them, as well as listing definitions and other key information.",
    imageSrc: "https://i.ibb.co/xfTm8N3/Containers.jpg",
    url: "https://horoserp.github.io/bootstrap/containers.html",
  },
